a9e38d66f40a2c2f1c5f6f41af55a07c3d32d23f
Walter Terrazas <walter.atg@gmail.com>
2018-04-28 15:01:07 -0400

    Header and footer

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 
 
