661adfd6407ba7d8eaf638e0a57925161d0e28e8
Walter Terrazas <walter.atg@gmail.com>
2018-04-29 18:09:55 -0400

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import {PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -28,19 +30,24 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
 	BrowserAnimationsModule,
     MaterialModule,
 	FlexLayoutModule,
-	AppRoutingModule
+	AppRoutingModule,
+	FormsModule
   ],
   providers: [
 	DishService,
 	PromotionService
 	],
+  entryComponents: [
+	LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
